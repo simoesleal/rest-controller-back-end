eaf20a88cc154b56b37881902017b3f0dbd89a03
Antonio Simões <antonio.leal@huper.tech>
2019-10-02 01:10:13 -0300

    fix/changed/payment-condition-fields

--- src/api/payment_condition/payment_condition.validation.js
+++ src/api/payment_condition/payment_condition.validation.js
@@ -1,22 +1,21 @@
 const DefaultError = require('../../handlers/default-error.handler')
 const { validateParam } = require('../utils/functions')
 
-async function validateNewPaymentCondition (condition, installments, firstDay, description, id_forma_pagamento) {
-  if (!await validateParam(condition)) throw new DefaultError(`Não identificamos a condição de pagamento e este é um campo obrigatório. Por favor, tente novamente.`, 'condition nulo ou vazio')
-  if (!await validateParam(installments)) throw new DefaultError(`Não identificamos as parcelas e este é um campo obrigatório. Por favor, tente novamente.`, 'installments nulo ou vazio')
-  if (!await validateParam(firstDay)) throw new DefaultError(`Não identificamos a data do pagamento e este é um campo obrigatório. Por favor, tente novamente.`, 'firstDay nulo ou vazio')
-  if (!await validateParam(description)) throw new DefaultError(`Não identificamos a descrição de pagamento e este é um campo obrigatório. Por favor, tente novamente.`, 'description nulo ou vazio')
+async function validateNewPaymentCondition (id_forma_pagamento, installmentNumber, issueDate, dueDate, installmentValue) {
   if (!await validateParam(id_forma_pagamento)) throw new DefaultError(`Não identificamos a forma de pagamento e este é um campo obrigatório. Por favor, tente novamente.`, 'id_forma_pagamento nulo ou vazio')
-
+  if (!await validateParam(installmentNumber)) throw new DefaultError(`Não identificamos o número da parcela e este é um campo obrigatório. Por favor, tente novamente.`, 'installmentNumber nulo ou vazio')
+  if (!await validateParam(issueDate)) throw new DefaultError(`Não identificamos a data de emissão da parcela e este é um campo obrigatório. Por favor, tente novamente.`, 'issueDate nulo ou vazio')
+  if (!await validateParam(dueDate)) throw new DefaultError(`Não identificamos a data do vencimento da parcela e este é um campo obrigatório. Por favor, tente novamente.`, 'dueDate nulo ou vazio')
+  if (!await validateParam(installmentValue)) throw new DefaultError(`Não identificamos o valor da parcela e este é um campo obrigatório. Por favor, tente novamente.`, 'installmentValue nulo ou vazio')
 }
 
-async function validateUpdatePaymentCondition (id,  condition, installments, firstDay, description, id_forma_pagamento) {
+async function validateUpdatePaymentCondition (id,  id_forma_pagamento, installmentNumber, issueDate, dueDate, installmentValue) {
   if (!await validateParam(id)) throw new DefaultError(`Não identificamos o código da Condição de pagamento e este é um campo obrigatório. Por favor, tente novamente.`, 'id nulo ou vazio')
-  if (!await validateParam(condition)) throw new DefaultError(`Não identificamos a condição de pagamento e este é um campo obrigatório. Por favor, tente novamente.`, 'condition nulo ou vazio')
-  if (!await validateParam(installments)) throw new DefaultError(`Não identificamos as parcelas e este é um campo obrigatório. Por favor, tente novamente.`, 'installments nulo ou vazio')
-  if (!await validateParam(firstDay)) throw new DefaultError(`Não identificamos a data do pagamento e este é um campo obrigatório. Por favor, tente novamente.`, 'firstDay nulo ou vazio')
-  if (!await validateParam(description)) throw new DefaultError(`Não identificamos a descrição de pagamento e este é um campo obrigatório. Por favor, tente novamente.`, 'description nulo ou vazio')
   if (!await validateParam(id_forma_pagamento)) throw new DefaultError(`Não identificamos a forma de pagamento e este é um campo obrigatório. Por favor, tente novamente.`, 'id_forma_pagamento nulo ou vazio')
+  if (!await validateParam(installmentNumber)) throw new DefaultError(`Não identificamos o número da parcela e este é um campo obrigatório. Por favor, tente novamente.`, 'installmentNumber nulo ou vazio')
+  if (!await validateParam(issueDate)) throw new DefaultError(`Não identificamos a data de emissão da parcela e este é um campo obrigatório. Por favor, tente novamente.`, 'issueDate nulo ou vazio')
+  if (!await validateParam(dueDate)) throw new DefaultError(`Não identificamos a data do vencimento da parcela e este é um campo obrigatório. Por favor, tente novamente.`, 'dueDate nulo ou vazio')
+  if (!await validateParam(installmentValue)) throw new DefaultError(`Não identificamos o valor da parcela e este é um campo obrigatório. Por favor, tente novamente.`, 'installmentValue nulo ou vazio')
 }
 
 module.exports = {
